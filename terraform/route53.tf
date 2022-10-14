79eaf79b797f7acff6a8788e4ecc95d93c2c0cff
amazeddev <amazeddev@gmail.com>
2022-10-15 00:13:27 +0200

    terraform

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -4,7 +4,7 @@ resource "aws_route53_zone" "primary" {
 
 resource "aws_route53_record" "www" {
   zone_id = aws_route53_zone.primary.zone_id
-  name = var.domain_name
+  name = local.domain
   type = "A"
   alias {
     name = aws_cloudfront_distribution.prod_distribution.domain_name
