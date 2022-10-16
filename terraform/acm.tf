8b9d111f8090dda5470f0d0d82aecbdfd000d84d
amazeddev <amazeddev@gmail.com>
2022-10-16 22:07:27 +0200

    terraform

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -8,6 +8,7 @@ resource "aws_acm_certificate" "cert" {
 }
 
 resource "aws_acm_certificate_validation" "cert" {
+  provider = aws.acm_provider
   certificate_arn = aws_acm_certificate.cert.arn
   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
