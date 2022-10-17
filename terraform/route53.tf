e84d369e3d32fa4f6513a00b35acef8c7cfdaa51
amazeddev <amazeddev@gmail.com>
2022-10-17 07:55:46 +0200

    terraform

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -26,10 +26,19 @@ resource "aws_route53_record" "www-a" {
 }
 
 resource "aws_route53_record" "cert_validation" {
-  count = length(aws_acm_certificate.cert.domain_validation_options)
-  name = element(aws_acm_certificate.cert.domain_validation_options.*.resource_record_name, count.index)
-  type = element(aws_acm_certificate.cert.domain_validation_options.*.resource_record_type, count.index)
-  zone_id = aws_route53_zone.primary.zone_id
-  records = [element(aws_acm_certificate.cert.domain_validation_options.*.resource_record_value, count.index)]
-  ttl = 60
+  for_each = {
+    for dvo in aws_acm_certificate.ssl_certificate.domain_validation_options : dvo.domain_name => {
+      name    = dvo.resource_record_name
+      record  = dvo.resource_record_value
+      type    = dvo.resource_record_type
+      zone_id = aws_route53_zone.main.zone_id
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = each.value.zone_id
 }
