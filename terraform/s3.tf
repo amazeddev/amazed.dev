203fb6e811a7d0369c013d1ae08cf336fc60b6b3
amazeddev <amazeddev@gmail.com>
2022-10-17 18:53:40 +0200

    terraform

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,31 +1,65 @@
 resource "aws_s3_bucket" "www_bucket" {
   bucket = "www.${var.bucket_name}"
-  acl = "public-read"
   policy = templatefile("templates/s3-policy.json", { bucket = "www.${var.bucket_name}" })
 
   cors_rule {
-    allowed_headers = ["Authorization", "Content-Length"]
+    allowed_headers = ["*"]
     allowed_methods = ["GET", "POST"]
-    allowed_origins = ["https://www.${var.domain_name}"]
+    allowed_origins = ["*"]
     max_age_seconds = 3000
   }
 
-  website {
-    index_document = "index.html"
-    error_document = "index.html"
-  }
   tags = var.common_tags
+}
+resource "aws_s3_bucket_website_configuration" "www_bucket_config" {
+  bucket = aws_s3_bucket.www_bucket.bucket
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_acl" "www_bucket_acl" {
+  bucket = aws_s3_bucket.www_bucket.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_public_access_block" "www_bucket_access_block" {
+  bucket = aws_s3_bucket.www_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket" "root_bucket" {
   bucket = var.bucket_name
-  acl = "public-read"
   policy = templatefile("templates/s3-policy.json", { bucket = var.bucket_name })
 
-  website {
-    redirect_all_requests_to = "https://www.${var.domain_name}"
-  }
 
   tags = var.common_tags
 }
 
+
+resource "aws_s3_bucket_website_configuration" "root_bucket_config" {
+  bucket = aws_s3_bucket.root_bucket.bucket
+  redirect_all_requests_to {
+    host_name = var.domain_name
+    protocol = "https"
+  }
+}
+resource "aws_s3_bucket_acl" "root_bucket_acl" {
+  bucket = aws_s3_bucket.root_bucket.id
+  acl    = "public-read"
+}
+resource "aws_s3_bucket_public_access_block" "root_bucket_access_block" {
+  bucket = aws_s3_bucket.root_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
