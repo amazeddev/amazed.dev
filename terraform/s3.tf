85d2d7c66e69874a46a92dd43d698dc59675630e
amazeddev <amazeddev@gmail.com>
2022-10-17 15:30:48 +0200

    terraform

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -10,10 +10,6 @@ resource "aws_s3_bucket" "www_bucket" {
     max_age_seconds = 3000
   }
 
-  website {
-    index_document = "index.html"
-    error_document = "index.html"
-  }
   tags = var.common_tags
 }
 
@@ -29,3 +25,19 @@ resource "aws_s3_bucket" "root_bucket" {
   tags = var.common_tags
 }
 
+resource "aws_s3_bucket_website_configuration" "www_bucket_config" {
+  bucket = aws_s3_bucket.www_bucket.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_website_configuration" "root_bucket_config" {
+  bucket = aws_s3_bucket.root_bucket.bucket
+  redirect_all_requests_to = "https://www.${var.domain_name}"
+}
