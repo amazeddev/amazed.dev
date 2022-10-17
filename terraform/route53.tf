13e052dccab669f580473dc648e1f8c2ca5a8310
amazeddev <amazeddev@gmail.com>
2022-10-17 08:00:40 +0200

    terraform

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -27,11 +27,11 @@ resource "aws_route53_record" "www-a" {
 
 resource "aws_route53_record" "cert_validation" {
   for_each = {
-    for dvo in aws_acm_certificate.ssl_certificate.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
       name    = dvo.resource_record_name
       record  = dvo.resource_record_value
       type    = dvo.resource_record_type
-      zone_id = aws_route53_zone.main.zone_id
+      zone_id = aws_route53_zone.primary.zone_id
     }
   }
 
